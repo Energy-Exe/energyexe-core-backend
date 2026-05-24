83f355566ffa5c1d4b817723740ce7e608146667
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2026-05-24 23:46:36 +0600

    chore: rebase migration down_revision onto c8d9e0f1a2b3 (post-#70 merge)

--- alembic/versions/d1f2a3b4c5e6_add_commercial_revenue_columns.py
+++ alembic/versions/d1f2a3b4c5e6_add_commercial_revenue_columns.py
@@ -6,7 +6,7 @@ constraint proxy / lost-value columns. Per the reference pipeline
 commercial summary shows.
 
 Revision ID: d1f2a3b4c5e6
-Revises: e4a1c83d9b21
+Revises: c8d9e0f1a2b3
 Create Date: 2026-05-24
 """
 
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "d1f2a3b4c5e6"
-down_revision = "e4a1c83d9b21"
+down_revision = "c8d9e0f1a2b3"
 branch_labels = None
 depends_on = None
 
